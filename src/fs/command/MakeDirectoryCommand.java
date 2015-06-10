169efc67c6e72dcd766a620056c1d76baba5f449
Leonardo Madrigal <lmadrigal09@gmail.com>
2015-06-10 00:07:42 -0600

    Make directory command implemented.

--- src/fs/command/MakeDirectoryCommand.java
+++ src/fs/command/MakeDirectoryCommand.java
@@ -1,5 +1,10 @@
 package fs.command;
 
+import fs.App;
+import fs.Disk;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author José Andrés García Sáenz <jags9415@gmail.com>
@@ -13,6 +18,32 @@ public class MakeDirectoryCommand extends Command {
         if (args.length < 2) {
             reportSyntaxError();
             return;
+        }
+        
+        String path = args[1];
+        App app = App.getInstance();
+        Disk disk = app.getDisk();
+        
+        try
+        {
+            if(disk.exists(path))
+            {
+                System.out.print("There is already a directory with the same name. Do you want to override it? [y/n] ");
+                    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+                    String line = input.readLine();
+                    if (!line.equalsIgnoreCase("y")) 
+                    {
+                        return;
+                    }
+                    disk.deleteDirectory(path);
+                    disk.createDirectory(path);
+                    
+                    System.out.println("Directory created successfully.");
+            }
+        }
+        catch (Exception ex) 
+        {
+            reportError(ex.getMessage());
         }
     }
 
