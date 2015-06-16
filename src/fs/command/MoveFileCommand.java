f45d0f36b099235d1232a2f800230e74553007ac
Leonardo Madrigal <lmadrigal09@gmail.com>
2015-06-15 19:22:25 -0600

    Move file command implemented.

---
+++ src/fs/command/MoveFileCommand.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fs.command;
+
+import fs.App;
+import fs.Disk;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Leo
+ */
+public class MoveFileCommand extends Command{
+
+    public static final String COMMAND = "Move";
+    @Override
+    public void execute(String[] args) 
+    {
+        try 
+        {
+            if (args.length != 3)
+            {
+                reportSyntaxError();
+                return;
+            }
+            String oldPath = args[1];
+            String newPath = args[3];
+            App app = App.getInstance();
+            Disk disk = app.getDisk();
+            disk.moveFile(oldPath, newPath);
+        } 
+        catch (IOException ex) 
+        {
+            Logger.getLogger(MoveFileCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    protected String getName() 
+    {
+        return COMMAND;
+    }
+
+    @Override
+    protected String getDescription() 
+    {
+        return "Moves a file or directory to another directory.";
+    }
+
+    @Override
+    protected String getSyntax() 
+    {
+        return getName() + "CurrentDirName /to/ NewDirName";
+    }
+    
+}
